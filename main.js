fedc9d821f932f51cb3cab4e4f6f65ac6f0e7a89
NureAlamShuvo <nurealamshuvo11@gmail.com>
2026-03-07 12:37:37 +0600

    Dynamic card styled based on priority and status

--- main.js
+++ main.js
@@ -25,11 +25,32 @@ function displayIssueCard (cards) {
         });
 
         const newCard = document.createElement("div");
-        newCard.className = "space-y-4 shadow-md p-4";
+        newCard.className = "";
+
+        if(card.status === "open"){
+            newCard.className ="space-y-4 shadow-md p-4 border-t-4 rounded-t-lg border-t-green-700";
+        }
+        else{
+            newCard.className = "space-y-4 shadow-md p-4 border-t-4 rounded-t-lg border-t-purple-700";
+        }
+
+        let priorityBadge = "";
+
+        if(card.priority === "high"){
+            priorityBadge = "badge badge-dash badge-success";
+        }
+        else if (card.priority === "medium"){
+            priorityBadge = "badge badge-dash badge-warning";
+        }
+        else{
+            priorityBadge = "badge badge-dash badge-error";
+        }
+
         newCard.innerHTML = `
         <div class="flex justify-between">
-                    <img src="./assets/Open-Status.png" alt="" class="h-6">
-                    <span class="badge badge-outline badge-success">${card.priority.toLocaleUpperCase()}</span>
+                    <img src="${card.status === "open" ? './assets/Open-Status.png' : './assets/Closed- Status .png'}" alt="" class="h-6">
+                    
+                    <span class="${priorityBadge}">${card.priority.toLocaleUpperCase()}</span>
                 </div>
 
                 
@@ -43,6 +64,7 @@ function displayIssueCard (cards) {
                 <hr class="text-gray-400">
                 <div class="space-y-2">
                     <p class="text-[#64748B]">#${card.author}</p>
+                    <p class="text-[#64748B]">Assignee : ${card.assignee ? card.assignee : "Unassigned"}</p>
                     <p class="text-[#64748B]">${dateConverUs}</p>
                 </div>
         `
