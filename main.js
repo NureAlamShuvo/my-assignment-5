8927f059d4b8470ad07030a698e032033adce9d1
NureAlamShuvo <nurealamshuvo11@gmail.com>
2026-03-07 15:43:29 +0600

    I fitered and showed the issueCard with dynamic issue count

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 const cardContainer = document.getElementById("card-container");
 const loadingSpinner = document.getElementById("loading-spinner");
 
+const btnAll = document.getElementById("btn-all");
+const btnOpen = document.getElementById("btn-open");
+const btnClosed = document.getElementById("btn-closed");
+const btnIssue = document.getElementById("issue-count");
+
+let allCards = [];
+
 
 // Issue card load function
 async function issueCardLoad() {
 
     loadingSpinner.classList.remove("hidden");
+    loadingSpinner.classList.add("flex");
 
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     const data = await res.json();
     
-    displayIssueCard(data.data);
+    allCards = data.data;
+    
+    displayIssueCard(allCards);
 
     loadingSpinner.classList.add("hidden");
 };
 
 function displayIssueCard (cards) {
+
+    cardContainer.innerHTML = "";
+
+    btnIssue.innerText = `${cards.length} Issues`;
 
 
     cards.forEach((card) => {
@@ -71,11 +85,37 @@ function displayIssueCard (cards) {
                 <div class="space-y-2">
                     <p class="text-[#64748B]">#${card.author}</p>
                     <p class="text-[#64748B]">Assignee : ${card.assignee ? card.assignee : "Unassigned"}</p>
-                    <p class="text-[#64748B]">${dateConverUs}</p>
+                    <p class="text-[#64748B]">Updated : ${dateConverUs}</p>
                 </div>
         `
         cardContainer.appendChild(newCard);
     });
 };
+
+// toggle btn function
+function toggleBtn (btnActive) {
+    btnAll.className = "btn btn-soft";
+    btnOpen.className = "btn btn-soft";
+    btnClosed.className = "btn btn-soft";
+    btnActive.className = "btn btn-primary";
+};
+
+// buttons click events
+btnAll.addEventListener("click", function(){
+    toggleBtn(btnAll);
+    displayIssueCard(allCards);
+});
+
+btnOpen.addEventListener("click", function(){
+    toggleBtn(btnOpen);
+    const openCards = allCards.filter(card => card.status === "open");
+    displayIssueCard(openCards);
+});
+
+btnClosed.addEventListener("click", function(){
+    toggleBtn(btnClosed);
+    const closedCards = allCards.filter(card => card.status === "closed");
+    displayIssueCard(closedCards);
+});
 
 issueCardLoad();
